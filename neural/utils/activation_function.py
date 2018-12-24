a856aad33c2e8e4b761937836f30c32f1cbd2ae0
wangfan <wangfan980@163.com>
2018-12-24 10:44:37 +0800

    add

--- neural/utils/activation_function.py
+++ neural/utils/activation_function.py
@@ -1,21 +1,24 @@
 # activation function
 import numpy as np
 
+
 # sigmoid function
 def sigmoid(x):
-	return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 # RELU function
 def relu(x):
-	return np.maximum(0, x)
+    return np.maximum(0, x)
+
 
 # softmax function
 def softmax(x):
-	exp_x = np.exp(x)
-	sum_exp_x = np.sum(exp_x)
-	y = exp_x / sum_exp_x
-	return y
+    exp_x = np.exp(x)
+    sum_exp_x = np.sum(exp_x)
+    y = exp_x / sum_exp_x
+    return y
 
 
 if __name__ == "__main__":
-	pass
+    pass
